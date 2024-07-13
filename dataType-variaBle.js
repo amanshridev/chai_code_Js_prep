f99dd7c6efd29c02b8133463b771805ee4771447
amanshridev <mailtoamanshri@proton.me>
2024-07-13 13:11:13 +0530

    Chai and Code JavaScript Challage Task 1:

---
+++ dataType-variaBle.js
@@ -0,0 +1,70 @@
+// console.log("Hi, with Bun");
+
+// Task 1: Var Declaration
+
+var varDecl = 10;
+
+// console.log("Task 1: ",varDecl)
+
+// Task 2: Let Declaration
+
+let letDecl = "Let Declaration";
+
+// console.log("Task 2: ", letDecl)
+
+// Task 3: Const Declaration
+
+const constDecl = true;
+
+// console.log("Task 3: ", constDecl);
+
+// Task 4: Multi Variable Declaration with typeof
+
+let typeOneNumber = 18;
+
+// console.log("Task 4: ", typeof typeOneNumber);
+
+let typeTwoString = "Hello! from Type Two";
+
+// console.log("Task 4: ", typeof typeTwoString);
+
+let typeThreeBoolen = false;
+
+// console.log("Task 4: ", typeof typeThreeBoolen);
+
+let typeFourObject = {
+    name: "Aman",
+  age: 18,
+};
+
+// console.log("Task 4: ", typeof typeFourObject);
+
+let typeFiveArray = [ 1, 2, 3, 4];
+
+// console.log("Task 4: ", typeof typeFiveArray);
+
+
+
+// Task 5: reassigning Values to let
+
+let reAssign = 22;
+
+// console.log("Task 5: ", reAssign); //before reassigning
+
+reAssign = "reassigning string value";
+
+// console.log("Task 5: ", reAssign); //after reassigning
+
+
+
+// Task 6: reassigning Values to const
+
+const reAssignConst = 25;
+
+console.log("Task 6: ", reAssignConst); //before reassigning
+
+reAssignConst = "New Value Assigned";
+
+console.log("Task 6: ", reAssignConst); //after reassigning
+
+// Task 6 OutPut: The symbol "reAssignConst" was already declared
